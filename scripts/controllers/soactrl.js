5903ecf6240c2bc7b236b67697437d74089bddbf
JANINE <nicegal17@gmail.com>
2016-01-23 14:53:16 +0800

    refactor config and soa

--- scripts/controllers/soactrl.js
+++ scripts/controllers/soactrl.js
@@ -1,18 +1,13 @@
 'use strict';
 
- angular.module('accounting')
-     .controller('soactrl', function($scope, $stateParams, $filter, $window, SOAFactory, toastr, ReportingService) {
+angular.module('accounting')
+    .controller('soactrl', function($scope, $stateParams, $filter, $window, SOAFactory, toaster, ReportingService) {
 
-         if (!_.isUndefined($stateParams.id)) {
-             SOAFactory.getPoDetails($stateParams.id).then(function(data) {
-                 if (data.length > 0) {
-                     $scope.soaDetails = data;
-                 }
-             });
-         }
-       
-         $scope.printData = function() {
-             var divToPrint = document.getElementById('printTable');
-             ReportingService.printData(divToPrint);
-         };
-     });
+        if (!_.isUndefined($stateParams.id)) {
+            SOAFactory.getInfo($stateParams.id).then(function(data) {
+                if (data.length > 0) {
+                    $scope.soaDetails = data;
+                }
+            });
+        }
+    })
